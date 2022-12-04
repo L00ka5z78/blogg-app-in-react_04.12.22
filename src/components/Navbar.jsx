897f30dfaa1d81370005fdb17dfe91ea20663e70
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-12-04 16:19:07 +0100

    home page simngle page features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../img/logo.png';
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="logo">logo</div>
-        <div className="links">links</div>
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            {/* redirects to http://localhost:3000/?cat=art */}
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>JOHN USER</span>
+          <span>Logout</span>
+
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import Edit from '../img/edit.png';
+import Delete from '../img/delete.png';
+import { Link } from 'react-router-dom';
 
 export default function Single() {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            {/* links to editing post number */}
+            <Link to={`/write/?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>
+          Lorem ipsum er opprinnelig et lettere redigert utdrag fra De finibus
+          bonorum et malorum av Cicero. Opprinnelig begynte avsnittet: Neque
+          porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+          adipisci velit.
+        </h1>
+        <p>
+          Lorem ipsum er opprinnelig et lettere redigert utdrag fra De finibus
+          bonorum et malorum av Cicero. Opprinnelig begynte avsnittet: Neque
+          porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur,
+          adipisci velit.
+        </p>
+      </div>
+      <div className="menu">mwnu</div>
+    </div>
+  );
 }
