10d1d9bf41a63284852190b6af6bc178da065870
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-12-10 20:47:07 +0100

    comiit

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 import express from "express";
+import { register, login, logout } from "../controllers/authController.js"
+
 
 const router = express.Router();
 
 router.post("/register")    // post method/create register endpoint/ath
+router.post("/login", login)
+router.post("/logout", logout)
 
 export default router;
