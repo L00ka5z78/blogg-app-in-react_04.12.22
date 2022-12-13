f0285d25e3b6aa2fddf4a5c65b65b5e52faa742a
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-12-13 19:17:08 +0100

    proxy error

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,58 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Register() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    //to avoid refreshing page
+    e.preventDefault();
+
+    try {
+      const res = await axios.post('/auth/register', inputs);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="text" placeholder="password" />
-        <button>Register</button>
+        <input
+          required
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Register</button>
+
         <p>This is error!</p>
         <span>
           {/* links user to register page */}
